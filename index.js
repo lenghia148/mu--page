a50d281f316d820ebad733e882177064e37ef6d9
unknown <ledinhnghia.qb@gmail.com>
2022-05-25 12:18:30 +0700

    update

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const slideImage = [
     },
 ]
  const scrSlide = document.querySelector('.slide__img')
- console.log(scrSlide.scr);
+
  
     function slideRandom()
     {
@@ -27,4 +27,77 @@ const slideImage = [
 
     setInterval(slideRandom,3000)
 
+    // GOALKEEPERS
+
+    const goalKeeper = 
+    [
+        {
+            number : 1,
+            namePlayer : "DAVID DEGEA",
+            imgPlayer  : "https://assets.manutd.com/AssetPicker/images/0/0/16/59/1063877/De_Gea_profile_pic_trial_edit1643296725957.jpg"
+        },
+        {
+            number : 26,
+            namePlayer : "DEAN HENDERSON",
+            imgPlayer  : "https://assets.manutd.com/AssetPicker/images/0/0/16/58/1063672/Henderson_profile_picture_edit_1643204896600.jpg"
+        } 
+    ] 
+
+    const defenders = 
+    [
+        {
+            number : 1,
+            namePlayer : "DAVID DEGEA",
+            imgPlayer  : "https://assets.manutd.com/AssetPicker/images/0/0/16/59/1063877/De_Gea_profile_pic_trial_edit1643296725957.jpg"
+        },
+        {
+            number : 26,
+            namePlayer : "DEAN HENDERSON",
+            imgPlayer  : "https://assets.manutd.com/AssetPicker/images/0/0/16/58/1063672/Henderson_profile_picture_edit_1643204896600.jpg"
+        } 
+    ] 
+    const midfielders = 
+    [
+        {
+            number : 1,
+            namePlayer : "DAVID DEGEA",
+            imgPlayer  : "https://assets.manutd.com/AssetPicker/images/0/0/16/59/1063877/De_Gea_profile_pic_trial_edit1643296725957.jpg"
+        },
+        {
+            number : 26,
+            namePlayer : "DEAN HENDERSON",
+            imgPlayer  : "https://assets.manutd.com/AssetPicker/images/0/0/16/58/1063672/Henderson_profile_picture_edit_1643204896600.jpg"
+        } 
+    ] 
+    const forwards = 
+    [
+        {
+            number : 1,
+            namePlayer : "DAVID DEGEA",
+            imgPlayer  : "https://assets.manutd.com/AssetPicker/images/0/0/16/59/1063877/De_Gea_profile_pic_trial_edit1643296725957.jpg"
+        },
+        {
+            number : 26,
+            namePlayer : "DEAN HENDERSON",
+            imgPlayer  : "https://assets.manutd.com/AssetPicker/images/0/0/16/58/1063672/Henderson_profile_picture_edit_1643204896600.jpg"
+        } 
+    ] 
+     
+        const goalKeeperPlayer = document.querySelector('.goalkeepers')
+        console.log(goalKeeperPlayer)
+
+        function showPlayers(postion,postionArr)
+
+        {
+            for(let i =0; i<postionArr.length;i++)
+            {
+                postion.innerHTML += `<div class="showplayers" style="background-image:url(${postionArr[i].imgPlayer});">
+               <span class="showplayers_number" > ${postionArr[i].number}</span>
+               <span class="showplayers_name" " > ${postionArr[i].namePlayer}</span>
+                </div>` 
+            }
+        }
+
+
+        showPlayers(goalKeeperPlayer,goalKeeper);
   
